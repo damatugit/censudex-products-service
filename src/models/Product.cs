7572c4be7b475d7a8943f7f2610081ecdb831fd2
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2025-10-31 12:00:28 -0300

    feat: product model created

---
+++ src/models/Product.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TallerDos.ProductsService.src.models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; } = null!;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+
+        [BsonElement("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [BsonElement("imagePublicId")]
+        public string ImagePublicId { get; set; }
+
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; } = true;
+        
+        [BsonElement("createdAt")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
